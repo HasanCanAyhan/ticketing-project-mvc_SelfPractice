aadff99423d4029cb629748f7cc9a05493489018
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-30 18:10:39 +0200

    ticketing project part2

--- src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -1,6 +1,7 @@
 package com.cydeo.boostrap;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.CrudService;
 import com.cydeo.service.RoleService;
 import org.springframework.boot.CommandLineRunner;
 
@@ -10,7 +11,7 @@ public class DataGenerator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception { // when you executed application, it will be run implicitly
 
-        //Iniatially
+        //Initially
 
         //This run method will execute FIRST BEFORE ANYTHING when you start the application
 
@@ -20,6 +21,25 @@ public class DataGenerator implements CommandLineRunner {
         RoleDTO managerRole = new RoleDTO(2L,"Manager");
         RoleDTO employeeRole = new RoleDTO(3L,"Employee");
 
+
+        //RoleDTO object-->> List<RoleDTO>  roleList
+        // --->> RoleService DependencyInjection --->> roleService.createAll(roleList)
+        //--->>CrudService  List<T> createAll(List<T> list);
+        //-->> AbstractMapService
+        /*
+          T create(ID id, T object) {
+                map.put(id, object);
+           return object;
+          }
+         */
+
+        //RoleService Impl
+        /*
+         @Override
+        public Region create(Region object) {
+            return super.create(object.getId(), object);
+        }
+         */
 
 
     }
