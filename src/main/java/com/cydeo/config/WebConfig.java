1b1c26c8f807cab0916eb668776aef96285d10d1
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-30 14:11:42 +0200

    ticketing project part1

--- src/main/java/com/cydeo/config/WebConfig.java
+++ src/main/java/com/cydeo/config/WebConfig.java
@@ -7,11 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // those are static pages , there is no here any data etc...., just show
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/welcome").setViewName("welcome");
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/").setViewName("login"); // localhost:8080
     }
 
 }
