84a6c84e7e401ff4b86a1cae8b0f88c65476a00e
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-30 17:15:10 +0200

    ticketing project part1

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    /*
+    private final RoleService roleService;
+
+    public UserController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+     */
+
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
         //model.addAttribute("roles", find all roles from db ); // all roles will come from DataBase
+        //model.addAttribute("roles", roleService.findAll());
 
         return "/user/create";
     }
--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,8 +1,11 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
+import org.springframework.stereotype.Service;
 
 //                                                       unique is Long :id
+
+
 public interface RoleService extends CrudService<RoleDTO,Long>{
 
 }
