c60062f677b10ccbe95b4b55f615ba7856aa341d
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-30 22:44:11 +0200

    ticketing project part2

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -2,11 +2,14 @@ package com.cydeo.controller;
 
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,12 +26,30 @@ public class UserController {
 
     @GetMapping("/create")
     public String createUser(Model model){
-        model.addAttribute("userList", userService.findAll());
         model.addAttribute("user", new UserDTO());
+
         model.addAttribute("roles", roleService.findAll() ); // all roles will come from DataBase
+
+        model.addAttribute("users", userService.findAll());
+
         return "/user/create";
+        // what ever inside the view, you have to create data for that
+        //what ever view needs (object,list etc) - you need to provide it
     }
 
+
+    //save button -->> post action --->> then we have user object from the UI , then we are holding it with ModelAttribute
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user") UserDTO user , Model model){
+
+        //go to create html and provides whatever needs it (user object, roles, users)
+        model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles", roleService.findAll() );
+
+        userService.save(user);
+        model.addAttribute("users", userService.findAll());
+        return "/user/create";
+    }
 
 
 }
