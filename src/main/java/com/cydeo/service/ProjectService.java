0df686583368640316bc9c0ea079a74f7982ce03
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-02 21:05:10 +0200

    ticketing project part3

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+
+//                                                              projectCode is unique : String
+public interface ProjectService extends CrudService<ProjectDTO,String> {
+
+
+
+}
