551d2398a2f44fd1e3310ffc059794e415629f75
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-04 22:28:07 +0200

    ticketing project part5

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
@@ -107,6 +108,54 @@ public class TaskController {
 
         return "redirect:/task/create";
     }
+
+
+
+    @GetMapping("/employee/pending-tasks")
+    public String employeePendingTasks(Model model){ // task which not completed
+
+        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+
+        return "/task/pending-tasks";
+    }
+
+    @GetMapping("/employee/archive")
+    public String employeeArchivedTasks(Model model){ // task which completed
+
+        model.addAttribute("tasks", taskService.findAllTasksByStatus(Status.COMPLETE));
+
+        return "/task/archive";
+    }
+
+
+    @GetMapping("/employee/edit/{id}")
+    public String employeeEditTask(@PathVariable Long id,Model model){
+
+        model.addAttribute("task", taskService.findById(id));
+
+        //model.addAttribute("projects", projectService.findAll());
+        //model.addAttribute("employees", userService.findEmployees());
+
+
+
+        model.addAttribute("statuses", Status.values());
+
+        model.addAttribute("tasks",taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+
+        return "/task/status-update";
+    }
+
+    // employee/update/{id}
+
+    @PostMapping("/employee/update/{id}")
+    public String employeeUpdateTask(TaskDTO task){
+        taskService.updateStatus(task);
+
+        return "redirect:/task/employee/pending-tasks";
+
+
+    }
+
 
 
 
